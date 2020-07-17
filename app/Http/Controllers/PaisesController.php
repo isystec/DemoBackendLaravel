79a497f4fa9bcd956b6586269f24da323dc49029
RedJohn1426 <alex98rodriguezromero@gmail.com>
2020-07-16 23:05:49 -0500

    se modifica api de lista de paises

--- app/Http/Controllers/PaisesController.php
+++ app/Http/Controllers/PaisesController.php
@@ -2,13 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use DB;
 use App\Paises;
 
 class PaisesController extends Controller
 {
-    public function index()
+    public function APIListPaises()
     {
         $data = new Paises;
         return $data::all(); 
